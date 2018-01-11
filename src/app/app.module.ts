e8bd56c93d5dd3c2cdca2be02e65caf5d071a1f9
Eric N. Garcia <engarcia618@gmail.com>
2018-01-11 12:04:15 -0500

    Created basic home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { HomeComponent } from './home/home.component';
+
+import { MatToolbarModule } from '@angular/material';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    RouterModule.forRoot(routes),
+    BrowserModule, BrowserAnimationsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
