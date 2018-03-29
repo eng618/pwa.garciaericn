7c93ddcd9562e55574ed6f39739e976e9c7821fc
Eric N. Garcia <engarcia618@gmail.com>
2018-03-29 01:46:08 -0400

    added missing ,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,7 @@ import { ContactComponent } from './contact/contact.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'contact' component: ContactComponent }
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
