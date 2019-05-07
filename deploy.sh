026836bf52cfde67b0ec6df72f1663cd865437b4
Eric N. Garcia <engarcia618@gmail.com>
2019-05-07 14:45:26 -0400

    💚  Testing new CI deply

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+echo "Current branch is $TRAVIS_BRANCH"
+echo "isPullRequest: $TRAVIS_PULL_REQUEST"
+
 # Verifys if branch is master and not a pull request.
 if [ "$TRAVIS_BRANCH" == "master" ] && [ ! "$TRAVIS_PULL_REQUEST" ]; then
   echo "Deploying to Firebase hosting"
-  firebase deploy --token $FIREBASE_TOKEN
+  firebase deploy --token "$FIREBASE_TOKEN"
 else
   echo "No deployment required"
 fi
