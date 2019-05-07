6c1f6391aacfa834ca90f50292e6420841f3e70b
Eric N. Garcia <engarcia618@gmail.com>
2019-05-07 15:31:39 -0400

    💚  Testing CI Deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ echo "isPullRequest: $TRAVIS_PULL_REQUEST"
 # Verifys if branch is master and not a pull request.
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying to Firebase hosting"
-  firebase deploy --token "$FIREBASE_TOKEN"
+  # firebase deploy --token "$FIREBASE_TOKEN"
+  yarn deploy
 else
   echo "No deployment required"
 fi
