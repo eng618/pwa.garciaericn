0e7d8eaac36426ea177be7e591a03ae58a2d2adb
Eric N. Garcia <engarcia618@gmail.com>
2019-05-07 15:29:29 -0400

    ⏪  Reverting changes

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,10 @@
 echo "Current branch is $TRAVIS_BRANCH"
 echo "isPullRequest: $TRAVIS_PULL_REQUEST"
 
-if [ "$staging" ]; then
-  echo "On $staging, no deployment required"
-fi
-
-if [ "$production" ]; then
-  # Verifys if branch is master and not a pull request.
-  if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo "Deploying to Firebase hosting"
-    firebase deploy --token "$FIREBASE_TOKEN"
-  else
-    echo "No deployment required"
-  fi
+# Verifys if branch is master and not a pull request.
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  echo "Deploying to Firebase hosting"
+  firebase deploy --token "$FIREBASE_TOKEN"
+else
+  echo "No deployment required"
 fi
