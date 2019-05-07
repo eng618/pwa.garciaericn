cbd18fbfaff8e85fc524301383cd7abd78f0445c
Eric N. Garcia <engarcia618@gmail.com>
2019-05-07 15:20:01 -0400

    Deployment tests

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,16 @@
 echo "Current branch is $TRAVIS_BRANCH"
 echo "isPullRequest: $TRAVIS_PULL_REQUEST"
 
-# Verifys if branch is master and not a pull request.
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  echo "Deploying to Firebase hosting"
-  firebase deploy --token "$FIREBASE_TOKEN"
-else
-  echo "No deployment required"
+if [ "$staging" ]; then
+  echo "On $staging, no deployment required"
+fi
+
+if [ "$production" ]; then
+  # Verifys if branch is master and not a pull request.
+  if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo "Deploying to Firebase hosting"
+    firebase deploy --token "$FIREBASE_TOKEN"
+  else
+    echo "No deployment required"
+  fi
 fi
