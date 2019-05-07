95f6d11bc89a7bb0ee3ab469651dbfcce904383b
Eric N. Garcia <engarcia618@gmail.com>
2019-05-07 14:57:04 -0400

    💚  Fixed email notification, and deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ echo "Current branch is $TRAVIS_BRANCH"
 echo "isPullRequest: $TRAVIS_PULL_REQUEST"
 
 # Verifys if branch is master and not a pull request.
-if [ "$TRAVIS_BRANCH" == "master" ] && [ ! "$TRAVIS_PULL_REQUEST" ]; then
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying to Firebase hosting"
   firebase deploy --token "$FIREBASE_TOKEN"
 else
