288f6903235894f924721c4a1e381d939ba609b1
Eric N. Garcia <engarcia618@gmail.com>
2019-05-08 08:34:04 -0400

    🚨  double quote var

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,12 @@ echo "isPullRequest: $TRAVIS_PULL_REQUEST"
 # Verifys if branch is master and not a pull request.
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying to Firebase hosting"
-  # firebase deploy --token "$FIREBASE_TOKEN"
   yarn global add firebase-tools
   yarn global add @angular/cli
   yarn
   ng build --prod
   firebase use default
-  firebase deploy --token $FIREBASE_TOKEN
+  firebase deploy --token "$FIREBASE_TOKEN"
 else
   echo "No deployment required"
 fi
