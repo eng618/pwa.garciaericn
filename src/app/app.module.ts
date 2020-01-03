9571a9d3b21c275269a6eea6457d8a47c533be8d
Eric N. Garcia <engarcia618@gmail.com>
2020-01-03 11:20:28 -0500

    chore(dependencies): Updated
    ng update and fixed errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,13 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { LayoutModule } from '@angular/cdk/layout';
 
 // Angular Material
-import {
-  MatButtonModule,
-  MatCardModule,
-  MatGridListModule,
-  MatIconModule,
-  MatListModule,
-  MatSidenavModule,
-  MatToolbarModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 // Firebase
 import { AngularFireModule } from 'angularfire2';
