a0d6df00e31097c1b46f0651de5e2355998e4c0a
Eric N. Garcia <engarcia618@gmail.com>
2020-06-08 14:06:43 -0400

    feat(localize): add ng localize.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 echo "Current branch is $TRAVIS_BRANCH"
 echo "isPullRequest: $TRAVIS_PULL_REQUEST"
+echo "env: ${1}"
 
-# Verifys if branch is master and not a pull request.
+# Verifies if branch is master and not a pull request.
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "Deploying to Firebase hosting"
   firebase use default
